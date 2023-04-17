7810f796e7fc91943b768aebb2c6cacb2f2bae84
Mark Gibson <mgibson@adaptavist.com>
2023-04-17 23:02:21 +0100

    Use storage for marketplace data

--- lib/marketplace.ts
+++ lib/marketplace.ts
@@ -1,4 +1,4 @@
-// deno-lint-ignore-file require-await
+import { getItem, listItems } from "$store";
 
 export interface Addon {
   id: string;
@@ -9,62 +9,46 @@ export interface Addon {
   markets?: string[];
 }
 
-export const config = {
-  addonBaseUrl: new URL("http://localhost:8100"),
-};
+const STORE = "marketplace";
 
 export async function listAddons(
   marketId: string,
 ): Promise<Addon[]> {
-  return hardcodedAddons()
-    .filter(({ markets }) => !markets || markets.includes(marketId));
+  const addons: Addon[] = [];
+
+  for await (const [, addon] of listItems<Addon>([STORE])) {
+    if (inMarket(marketId, addon)) {
+      addons.push(addon);
+    }
+  }
+
+  return addons;
 }
 
 export async function getAddon(
   marketId: string,
   addonId: string,
 ): Promise<Addon | undefined> {
-  return hardcodedAddons()
-    .find(({ id, markets }) =>
-      id === addonId && (!markets || markets.includes(marketId))
-    );
+  const addon = await getItem<Addon>([STORE, addonId]);
+  if (inMarket(marketId, addon)) {
+    return addon;
+  }
 }
 
 export async function listCategories(
   marketId: string,
 ): Promise<string[]> {
   const cats = new Set<string>();
-  for (const { markets, categories } of hardcodedAddons()) {
-    if (!markets || markets.includes(marketId)) {
-      categories.forEach((c) => cats.add(c));
+
+  for await (const [, addon] of listItems<Addon>([STORE])) {
+    if (inMarket(marketId, addon)) {
+      addon.categories.forEach((c) => cats.add(c));
     }
   }
+
   return [...cats];
 }
 
-function url(path: string) {
-  return new URL(path, config.addonBaseUrl).href;
-}
-
-// TODO: Move $store modules from hyper-hub into public deno.land/x space, and use it here
-
-function hardcodedAddons(): Addon[] {
-  return [
-    {
-      id: "quiz",
-      title: "Quiz",
-      description: "A fun embeddable Quiz using the Trivia API",
-      augmentation: url("/quiz/addon.css"),
-      categories: ["Fun", "Example", "Games"],
-      markets: ["ref", "gitlab", "backstage"],
-    },
-    {
-      id: "splats",
-      title: "Project Splats",
-      description: "Display version/tag/commit next to each project",
-      augmentation: url("/splats/addon.css"),
-      categories: ["Example", "Projects", "Git"],
-      markets: ["gitlab"],
-    },
-  ];
+function inMarket(marketId: string, addon?: Addon) {
+  return addon && (!addon.markets || addon.markets.includes(marketId));
 }
